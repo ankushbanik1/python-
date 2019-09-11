dd7dab663e26a02b80e2b0b95d2c3b2b1ba55b1f
ankush banik <ankushbanik123@gmail.com>
2019-09-11 23:48:11 +0530

    autogui

---
+++ Advanced Python/pyautogui1.py
@@ -0,0 +1,7 @@
+import pyautogui
+#Point(x=467, y=62)
+print(pyautogui.position())
+pyautogui.click(467,62)
+pyautogui.typewrite("ankush banik")
+pyautogui.typewrite(["enter"])
+# pyautogui.hotkey("ctrl","c")
---
+++ Advanced Python/string_manupulation.py
@@ -0,0 +1,14 @@
+sent="hello i am ankush \n"
+print(sent) # \n is now printing
+
+sent =r'hello i \n am ankush'
+print(sent) #print \n as well by r''
+sent1="hello "
+ss=sent1.upper()#making this upperclass
+print(ss)
+sent1.islower()#chechking is it lower
+sentu="ruby is working alone"
+spt=sentu.split(' ')
+print(spt)
+sr=' '.join(spt) # joining list
+print(sr)
